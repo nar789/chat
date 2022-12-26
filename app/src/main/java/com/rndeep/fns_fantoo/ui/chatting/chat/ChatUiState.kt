6f26c1085bb84cf6983bbb0a43c56c4ddcf9dc9d
bllizard7891 <bllizard7891@gmail.com>
2022-12-26 23:08:00 +0900

    - stateList 버그 수정

--- app/src/main/java/com/rndeep/fns_fantoo/ui/chatting/chat/ChatUiState.kt
+++ app/src/main/java/com/rndeep/fns_fantoo/ui/chatting/chat/ChatUiState.kt
@@ -1,10 +1,11 @@
 package com.rndeep.fns_fantoo.ui.chatting.chat
 
+import androidx.compose.runtime.mutableStateListOf
 import com.rndeep.fns_fantoo.data.remote.model.chat.Message
 import com.rndeep.fns_fantoo.data.remote.model.chat.ReadInfo
 
 data class ChatUiState(
-    val messages: List<Message> = listOf(),
+    val messages: List<Message> = mutableStateListOf(),
     val myId: String = "",
     val translateMode: Boolean = false,
     val blocked: Boolean = false,
