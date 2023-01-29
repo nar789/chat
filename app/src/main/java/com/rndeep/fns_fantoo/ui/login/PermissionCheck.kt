8735061de2d5cef48cf3bf5f1f589a86799a753f
bllizard7891 <bllizard7891@gmail.com>
2023-01-30 01:42:43 +0900

    read_media_images permission

--- app/src/main/java/com/rndeep/fns_fantoo/ui/login/PermissionCheck.kt
+++ app/src/main/java/com/rndeep/fns_fantoo/ui/login/PermissionCheck.kt
@@ -2,18 +2,35 @@ package com.rndeep.fns_fantoo.ui.login
 
 import android.Manifest
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import pub.devrel.easypermissions.EasyPermissions
 
 class PermissionCheck {
 
-    companion object{
-        val PERMISSIONS_ARRAY = arrayOf(
+    companion object {
+        private val PERMISSIONS_ARRAY_UNDER_32 = arrayOf(
             Manifest.permission.CAMERA,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.RECORD_AUDIO)
+            Manifest.permission.RECORD_AUDIO
+        )
 
-        fun checkPermission(context:Context):Boolean{
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        private val PERMISSIONS_ARRAY_OVER_33 = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_MEDIA_IMAGES,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO
+        )
+
+        val PERMISSIONS_ARRAY = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            PERMISSIONS_ARRAY_OVER_33
+        } else {
+            PERMISSIONS_ARRAY_UNDER_32
+        }
+
+        fun checkPermission(context: Context): Boolean {
             return EasyPermissions.hasPermissions(context, *PERMISSIONS_ARRAY)
         }
     }
